b4b275f3889bf24aa6dff8760b62306530e19638
Petr Zalizko <pzalizko@gmail.com>
2024-08-06 19:55:32 +0300

    App runs

---
+++ AudioUploadApp/Controllers/AudioUploadController.cs
@@ -0,0 +1,32 @@
+﻿using AudioUploadApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AudioUploadApp.Controllers
+{
+    public class AudioUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(AudioFile model)
+        {
+            if (ModelState.IsValid)
+            {
+                var filePath = Path.Combine("wwwroot/uploads", model.File.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await model.File.CopyToAsync(stream);
+                }
+
+                ViewBag.Message = "Файл успешно загружен!";
+                return View("Index");
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ AudioUploadApp/Models/AudioFile.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AudioUploadApp.Models
+{
+    public class AudioFile
+    {
+        [Required]
+        public IFormFile File { get; set; }
+    }
+}
